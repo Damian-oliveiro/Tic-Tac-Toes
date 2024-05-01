5fe38d356497bb366047581d926c21d0b9f47bab
Damian-oliveiro <56987619+Damian-oliveiro@users.noreply.github.com>
2024-05-01 20:14:40 +0800

    814 pm

--- script.js
+++ script.js
@@ -51,5 +51,33 @@ function cheakWin(){
 }
 
 function cheakDraw(){
+    if(!isGameOver){
+        let isDraw  =true;
+        boxes.forEach(e =>{
+            if(e.innerHTML === "") isDraw = false;
+        })
 
+        if(isDraw){
+            isGameOver = true;
+            document.querySelector("#results").innerHTML = " Draw";
+            document.querySelector("#play-again").style.display = "inline"
+        }
+    }
 }
+
+
+
+// Issues with this segment of code, next time update//
+// document.querySelector("#play-again").addEventListener("click", ()=>{
+//     isGameOver = false;
+//     turn = "X";
+//     document.querySelector(".bg").style.left = "0";
+//     document.querySelector("#results").innerHTML = "";
+//     document.querySelector("play-again").style.display = "none";
+
+//     boxes.forEach(e =>{
+//         e.innerHTML = "";
+//         e.style.removeProperty("background-color");
+//         e.style.color = "FFF"
+//     })
+// })
