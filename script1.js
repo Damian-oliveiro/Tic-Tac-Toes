8c618c1ce5f24ea05728cc15de6ed745a8b55d18
Damian-oliveiro <56987619+Damian-oliveiro@users.noreply.github.com>
2024-05-21 18:00:52 +0800

    deleted old code

--- script1.js
+++ script1.js
@@ -26,7 +26,7 @@ const resetGame = () => {
     gameOver = false;
     statusText.textContent = `Player ${currentPlayer}'s turn`;
     gameContainer.innerHTML = '';
-    createBoard(10);
+    createBoard(3);
 };
 
 const placeMark = (x, y) => {
@@ -89,41 +89,3 @@ const handleClick = (event) => {
 };
 
 createBoard(3);
-
-
-// // create array to hold board data
-// let boardData = [
-// [0,0,0],
-// [0,0,0],
-// [0,0,0]
-// ]
-
-// // define game variables
-// let player = 1;
-
-
-// // pull in cells from DOM  
-// const cellElements = document.querySelectorAll(".cell");
-// console.log(cellElements);
-
-// // Add event listener
-// cellElements.forEach((cell, index) => {
-//     cell.addEventListener("click", () => {
-//         placeMarker(index);
-//     });
-// });
-
-// //create function for placing markers
-
-// function placeMarker(index){
-//     // Determine row and column from index
-//     let col = index % 3
-//     let row = (index - col) / 3
-//     // check if current cell is empty
-//     if (boardData[row][col] ==0){
-//     boardData[row][col] = player;
-//     // change player 
-//     player *= -1 ;
-//     console.log(boardData);
-//     }
-// }
